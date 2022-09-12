9c02b56825594850276c19a26503f63f297d08dd
mrhotio <26902309+mrhotio@users.noreply.github.com>
2022-09-12 12:31:36 +0200

    skip ci

--- update.sh
+++ update.sh
@@ -31,5 +31,6 @@ elif [[ ${1} == "screenshot" ]]; then
 else
     version=$(curl -fsSL "https://repo.jellyfin.org/releases/server/ubuntu/unstable/server/" | grep -o ">jellyfin-server_.*_arm64.deb<" | sed -e 's/>jellyfin-server_//g' -e 's/-unstable_arm64.deb<//g' | sort -r | head -1)
     [[ -z ${version} ]] && exit 1
-    echo '{"version":"'"${version}"'"}' | jq . > VERSION.json
+    version_json=$(cat ./VERSION.json)
+    jq '.version = "'"${version}"'"' <<< "${version_json}" > VERSION.json
 fi
