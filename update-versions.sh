ca21738ef6b40d583ff5b7cf4469fd5595f926e6
mrhotio <26902309+mrhotio@users.noreply.github.com>
2023-09-23 19:22:06 +0200

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-version=$(curl -fsSL "https://repo.jellyfin.org/releases/server/ubuntu/stable/server/" | grep -o ">jellyfin-server_.*_arm64.deb<" | sed -e 's/>jellyfin-server_//g' -e 's/_arm64.deb<//g' | sort -r | head -1)
+#version=$(curl -fsSL "https://repo.jellyfin.org/releases/server/ubuntu/stable/server/" | grep -o ">jellyfin-server_.*_arm64.deb<" | sed -e 's/>jellyfin-server_//g' -e 's/_arm64.deb<//g' | sort -r | head -1)
+version=$(curl -fsSL "https://repo.jellyfin.org/ubuntu/dists/jammy/main/binary-amd64/Packages" | grep -A 7 -m 1 'Package: jellyfin-server' | awk -F ': ' '/Version/{print $2;exit}')
 [[ -z ${version} ]] && exit 0
 intel_cr_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/intel/compute-runtime/releases/latest" | jq -r '.tag_name')
 [[ -z ${intel_cr_version} ]] && exit 0
