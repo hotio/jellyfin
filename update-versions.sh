e2b6ebd283ec17e4f6c955ce0bf8a079e143aa5e
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 08:32:20 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
-#version=$(curl -fsSL "https://repo.jellyfin.org/releases/server/ubuntu/stable/server/" | grep -o ">jellyfin-server_.*_arm64.deb<" | sed -e 's/>jellyfin-server_//g' -e 's/_arm64.deb<//g' | sort -r | head -1)
-version=$(curl -fsSL "https://repo.jellyfin.org/ubuntu/dists/jammy/main/binary-amd64/Packages" | grep -A 7 -m 1 'Package: jellyfin-server' | awk -F ': ' '/Version/{print $2;exit}')
-[[ -z ${version} ]] && exit 0
-intel_cr_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/intel/compute-runtime/releases/latest" | jq -r '.tag_name')
-[[ -z ${intel_cr_version} ]] && exit 0
-version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'"' <<< "${version_json}" > VERSION.json
-version_json=$(cat ./VERSION.json)
-jq '.intel_cr_version = "'"${intel_cr_version}"'"' <<< "${version_json}" > VERSION.json
+set -e
+version=$(curl -fsSL "https://repo.jellyfin.org/ubuntu/dists/jammy/main/binary-amd64/Packages" | grep -A 7 -m 1 'Package: jellyfin-server')
+version=$(awk -F ': ' '/Version/{print $2;exit}' <<< "${version}")
+intel_cr_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/intel/compute-runtime/releases/latest" | jq -re '.tag_name')
+json=$(cat VERSION.json)
+jq --sort-keys \
+    --arg version "${version//v/}" \
+    --arg intel_cr_version "${intel_cr_version//v/}" \
+    '.version = $version | .intel_cr_version = $intel_cr_version' <<< "${json}" | tee VERSION.json
