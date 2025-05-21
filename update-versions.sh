e529812229f4fa26db087375502311ac1dc0d79d
mrhotio <26902309+mrhotio@users.noreply.github.com>
2025-05-21 08:08:16 +0200

    fix intel

--- update-versions.sh
+++ update-versions.sh
@@ -2,10 +2,8 @@
 version=$(curl -fsSL "https://repo.jellyfin.org/ubuntu/dists/noble/unstable/binary-amd64/Packages" | grep -A 7 -m 1 'Package: jellyfin-server') || exit 1
 version=$(awk -F ': ' '/Version/{print $2;exit}' <<< "${version}" | sed s/+.*//g)
 intel_cr_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/intel/compute-runtime/releases/latest" | jq -re '.tag_name') || exit 1
-intel_gc_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/intel/intel-graphics-compiler/releases/latest" | jq -re '.tag_name') || exit 1
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
     --arg intel_cr_version "${intel_cr_version//v/}" \
-    --arg intel_gc_version "${intel_gc_version//v/}" \
-    '.version = $version | .intel_cr_version = $intel_cr_version | .intel_gc_version = $intel_gc_version' <<< "${json}" | tee VERSION.json
+    '.version = $version | .intel_cr_version = $intel_cr_version' <<< "${json}" | tee VERSION.json
