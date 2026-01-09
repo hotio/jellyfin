d041df0dc9aea942063e400578c3cc48a11ac158
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-09 08:45:00 +0100

    Update upstream tag SHA and improve error handling in update scripts

--- update-versions.sh
+++ update-versions.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-version=$(curl -fsSL "https://repo.jellyfin.org/ubuntu/dists/noble/main/binary-amd64/Packages" | grep -A 7 -m 1 'Package: jellyfin-server') || exit 1
+set -exuo pipefail
+
+version=$(curl -fsSL "https://repo.jellyfin.org/ubuntu/dists/noble/main/binary-amd64/Packages" | grep -A 7 -m 1 'Package: jellyfin-server')
 version=$(awk -F ': ' '/Version/{print $2;exit}' <<< "${version}" | sed s/+.*//g)
-intel_cr_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/intel/compute-runtime/releases/latest" | jq -re '.tag_name') || exit 1
+intel_cr_version=$(curl -fsSL "https://api.github.com/repos/intel/compute-runtime/releases/latest" | jq -re '.tag_name')
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
