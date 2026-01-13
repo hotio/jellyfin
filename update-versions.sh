15c9072fe62c54611d01f618bbbcb250176bb63b
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-13 09:55:38 +0100

    rename args [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -3,9 +3,9 @@ set -exuo pipefail
 
 version=$(curl -fsSL "https://repo.jellyfin.org/ubuntu/dists/noble/unstable/binary-amd64/Packages" | grep -A 7 -m 1 'Package: jellyfin-server')
 version=$(awk -F ': ' '/Version/{print $2;exit}' <<< "${version}" | sed s/+.*//g)
-intel_cr_version=$(curl -fsSL "https://api.github.com/repos/intel/compute-runtime/releases/latest" | jq -re '.tag_name')
+version_intel_cr=$(curl -fsSL "https://api.github.com/repos/intel/compute-runtime/releases/latest" | jq -re '.tag_name')
 json=$(cat meta.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
-    --arg intel_cr_version "${intel_cr_version//v/}" \
-    '.version = $version | .intel_cr_version = $intel_cr_version' <<< "${json}" | tee meta.json
+    --arg version_intel_cr "${version_intel_cr//v/}" \
+    '.version = $version | .version_intel_cr = $version_intel_cr' <<< "${json}" | tee meta.json
